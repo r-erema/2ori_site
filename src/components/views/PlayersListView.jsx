bf84d0890015cb0e4e92ed74a06fe59c91dd6490
Roman Erema <roman.erema@synesis.ru>
2019-12-02 10:40:54 +0300

    Margins

--- src/components/views/PlayersListView.jsx
+++ src/components/views/PlayersListView.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PlayerContainer from '../containers/PlayerContainer.jsx';
-
 import AddIcon from '@material-ui/icons/Add';
 import Button from '@material-ui/core/Button';
+import {withStyles} from "@material-ui/styles";
 
+const styles = theme => ({
+    root: {
+        '& > *': {
+            margin: 10,
+        },
+    },
+});
 
-export default class PlayersListView extends React.Component {
-
+class PlayersListView extends React.Component {
     render() {
-        let {players, tourneyTeamsCount, addPlayer} = this.props;
-
-        return <div>
+        let {players, tourneyTeamsCount, addPlayer, classes} = this.props;
+        return <div className={classes.root} >
             <Button variant="contained" color="primary" onClick={() => addPlayer(tourneyTeamsCount)} >
                 <AddIcon />
                 Add player
@@ -18,5 +23,7 @@ export default class PlayersListView extends React.Component {
             {players.map(player => <PlayerContainer player={player} key={player.id} />)}
         </div>;
     }
-
 }
+
+
+export default withStyles(styles)(PlayersListView);
