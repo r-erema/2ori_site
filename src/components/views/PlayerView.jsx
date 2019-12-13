e591c6b146071f080eb173d10b0483180d99a249
gutsout <eratut@mail.ru>
2019-12-13 19:21:40 +0300

    Progress

--- src/components/views/PlayerView.jsx
+++ src/components/views/PlayerView.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import RequiredTeamsListContainer from "../containers/RequiredTeamsListContainer.jsx";
-import Input from '@material-ui/core/Input';
 import TextField from '@material-ui/core/TextField';
+import {withStyles} from "@material-ui/styles";
 
+const styles = theme => ({
+    root: {
+        '& > *': {
+            "margin-bottom": "5%",
+        },
+    },
+});
 
-export default class PlayerView extends React.Component {
+class PlayerView extends React.Component {
 
     render() {
         const {
             player,
             updatePlayerName,
             updatePlayerTeamsCount,
-            tourneyTeamsCount
+            tourneyTeamsCount,
+            classes
         } = this.props;
         return (
             <Card>
                 <CardContent>
 
-                    <div>
+                    <div className={classes.root} >
                         <TextField
                             id="outlined-name"
                             label="Player name"
@@ -30,17 +38,18 @@ export default class PlayerView extends React.Component {
                         />
                     </div>
 
-                    <div>
-                        <Input
+                    <div className={classes.root}>
+                        <TextField
                             label="Teams count:"
                             type="number"
                             inputProps={{ min: 1 }}
                             value={player.teamsCount}
+                            variant="outlined"
                             onChange={(event) => {updatePlayerTeamsCount(player.id, event.target.value, tourneyTeamsCount, player.teamsCount)}}
                         />
                     </div>
 
-                    <RequiredTeamsListContainer player={player} />
+                    <RequiredTeamsListContainer  player={player} />
 
                 </CardContent>
             </Card>
@@ -48,3 +57,5 @@ export default class PlayerView extends React.Component {
     }
 
 }
+
+export default withStyles(styles)(PlayerView);
